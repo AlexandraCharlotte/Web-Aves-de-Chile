9292ea183ecbfec0a1c23af16b03bc1902611a60
AlexandraCharlotte <AlexandraCharlotte@users.noreply.github.com>
2022-12-26 16:47:58 -0300

    Add files via upload

---
+++ assets/js/mi_script.js
@@ -0,0 +1,37 @@
+/* COPIA Y PEGA TODO PORFIS!!!!! excepto el mensaje obvio xD */
+
+function showPi() {
+  var p = document.getElementById("tPicaflor");
+  if (p.style.display === "block") {
+    p.style.display = "none";
+  } else {
+    p.style.display = "block";
+  }
+}
+
+function showCo() {
+  var c = document.getElementById("tColores");
+  if (c.style.display === "block") {
+    c.style.display = "none";
+  } else {
+    c.style.display = "block";
+  }
+}
+
+function showGo() {
+  var g = document.getElementById("tGolondrina");
+  if (g.style.display === "block") {
+    g.style.display = "none";
+  } else {
+    g.style.display = "block";
+  }
+}
+
+function showMa() {
+  var m = document.getElementById("tMartin");
+  if (m.style.display === "block") {
+    m.style.display = "none";
+  } else {
+    m.style.display = "block";
+  }
+}
